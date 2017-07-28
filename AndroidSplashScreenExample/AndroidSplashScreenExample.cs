fb9d39a646834ac4ff15abe2a7fb124be5470fb3
Juan <juan.rios.28@gmail.com>
2017-07-27 23:16:56 -0400

    Splash screen xamarin example

---
+++ AndroidSplashScreenExample/AndroidSplashScreenExample.cs
@@ -0,0 +1,18 @@
+﻿    // (MainLauncher = true) makes the splash screen the first screen that is launched
+    // (No History = true) removes the Activity from BackStack when user navigates away from the Activity
+
+    [Activity(Label = "AndroidSplashScreenExample", MainLauncher = true, NoHistory = true)]
+
+    public class AndroidSplashScreenExample : Activity
+    {
+
+		protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            StartActivity(typeof(Place the activity you want to load up after the splash screen in here));
+
+            // Finishes the activity once user has left it
+            Finish();
+        }
+    }
