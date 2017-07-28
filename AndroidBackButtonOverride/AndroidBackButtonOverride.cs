7f424087e1c511231225c283818adadcff7f71a8
Juan <juan.rios.28@gmail.com>
2017-07-27 23:25:58 -0400

    Android back button example code

---
+++ AndroidBackButtonOverride/AndroidBackButtonOverride.cs
@@ -0,0 +1,8 @@
+// This method allows you to override the back button and either block it
+// from doing anything or have it perform an action such as starting another 
+// activity, or completely erasing the backstack 
+
+public override void OnBackPressed()
+{
+
+}
